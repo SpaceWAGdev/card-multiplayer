76ce7d1388428996524450d71576e27bc825c241
starlight_caffeine <starlight-caffeine@posteo.org>
2024-02-28 08:38:43 +0100

    Init Rust server

---
+++ server/target/debug/deps/server-d0ad6f1c525a88e7.d
@@ -0,0 +1,5 @@
+/home/starlight/Documents/dev/card-multiplayer/server/target/debug/deps/libserver-d0ad6f1c525a88e7.rmeta: src/main.rs
+
+/home/starlight/Documents/dev/card-multiplayer/server/target/debug/deps/server-d0ad6f1c525a88e7.d: src/main.rs
+
+src/main.rs:
