cb2b19eb00cea9b48ee1eb109547005ccd39f7c3
arthorias561 <buechold.noah@gmail.com>
2024-03-01 14:51:01 +0100

    Test on Decks and Temporarily deactivate leader ability

--- Cards/scripts/546f1255-432f-470e-b5b4-0bdcdec830fb.gd
+++ Cards/scripts/546f1255-432f-470e-b5b4-0bdcdec830fb.gd
@@ -4,5 +4,5 @@ extends CardBase
 func handleAbility(card: CardBase):
 	game_manager.move_card(card, "REMOTE_HAND")
 
-func leaderAbility():
-	GameState.get_card_selection()
+#func leaderAbility():
+	#GameState.get_card_selection()
