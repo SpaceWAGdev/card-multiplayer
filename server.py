46d19ccec19e02b9ce3738c356ea7118407107ef
starlight_caffeine <starlight-caffeine@posteo.org>
2024-04-19 15:17:32 +0200

    Naive matches implementation

--- server.py
+++ server.py
@@ -2,11 +2,14 @@
 import asyncio
 import websockets
 from hashlib import md5
+import uuid
 
 # Dictionary to store connected clients with their respective WebSocket objects
 connected_clients = {}
+matches = {}
 
-async def server(websocket, path):
+
+async def server(websocket: websockets.WebSocketServer, path):
     print(f"Client connected: {websocket.remote_address}")
     # Add the new client to the dictionary of connected clients
     connected_clients[websocket.remote_address] = websocket
@@ -15,14 +18,27 @@ async def server(websocket, path):
         async for message in websocket:
             print(f"Received message from {websocket.remote_address}: {message}")
 
-            # Echo the message back to every other connected client
-            for client_address, client in connected_clients.items():
-                if (
-                    client_address != websocket.remote_address
-                    and md5(message) != last_msg_hash
-                ):
-                    await client.send(message)
-            last_msg_hash = md5(message)
+            if message.startsWith("CREATE_MATCH_WITH_KEY"):
+                code: str = message.split(":")[1]
+                if code not in matches.keys():
+                    print(f"Created Match with code {code}")
+                    matches[code] = [websocket.remote_address]
+            elif message.startsWith("JOIN_MATCH_WITH_KEY"):
+                code: str = message.split(":")[1]
+                if code in matches.keys():
+                    matches[code].append(websocket.remote_address)
+            elif message.startsWith("MATCHMAKING"):
+                pass
+
+            else:
+                # Echo the message back to every other connected client in the same match
+                for client_address, client in connected_clients.items():
+                    if (client_address != websocket.remote_address 
+                        and md5(message) != last_msg_hash 
+                        and client_address in matches[message.split(":")[0]] ):
+                        
+                        await client.send(message)
+                        last_msg_hash = md5(message)
 
     except websockets.exceptions.ConnectionClosedError as e:
         print(f"Connection closed with error: {e}")
@@ -32,6 +48,8 @@ async def server(websocket, path):
 
         # Remove the disconnected client from the dictionary
         del connected_clients[websocket.remote_address]
+        for address_list in matches.values:
+            [address_list.remove(a) for a in address_list if a == websocket.remote_address]
 
 
 # Start the WebSocket server
